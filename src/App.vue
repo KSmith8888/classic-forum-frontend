251e5a850e76d01995563e325a6f6aef3d9146db
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2024-03-05 18:23:58 -0800

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HomePage from "./components/HomePage.vue";
+</script>
+
+<template>
+    <div>
+        <a href="#" class="test-link"> Test link </a>
+    </div>
+    <HomePage msg="Classic Forum Message" />
+</template>
+
+<style scoped>
+.test-link:hover {
+    color: green;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+    msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+
+    <div class="card">
+        <button type="button" @click="count++">count is {{ count }}</button>
+        <p>This is the HomePage component</p>
+    </div>
+
+    <p>This is the test paragraph</p>
+</template>
+
+<style scoped>
+.test {
+    color: #888;
+}
+</style>
